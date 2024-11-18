2942cc30b9b6ee329c75296fdc1f749099bad83b
mukeshkumarprajapti <prajaptimukeshkumar@gmail.com>
2024-11-18 08:40:24 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import { Button } from './components/ui/button'
+import { CarouselDemo } from './components/demo/CarouselDemo'
+
+function App() {
+
+  return (
+    <>
+    <div className="px-20">
+      
+    <Button>Hello Sir</Button>
+    <CarouselDemo></CarouselDemo>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
